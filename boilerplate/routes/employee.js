c21623cabfde527fcc6d0f1057b43c141a8773c4
Sowmya <sowmya.aji@gmail.com>
2019-07-09 21:44:01 -0400

    Add working joke and quote code to the post request

--- boilerplate/routes/employee.js
+++ boilerplate/routes/employee.js
@@ -2,14 +2,13 @@
 
 const express = require('express');
 const router = express.Router();
-const request = require("request");
+const request = require("request-promise-native");
 const cheerio = require("cheerio");
 // let dToday = new Date();
 // console.log(dToday)
 
 
 let maxId = 4
-
 
 const DATABASE = {
   employee1: {
@@ -46,12 +45,12 @@ const DATABASE = {
   },
 };
 
-/* GET employees listing. */
+// GET employees listing.
 router.get('', function (req, res) {
   return res.send(DATABASE);
 });
 
-/* GET one employee by id. */
+// GET one employee by id. 
 router.get('/:id', function (req, res) {
   const employee = DATABASE["employee" + req.params.id];
   if (req.params.id in DATABASE) {
@@ -62,32 +61,46 @@ router.get('/:id', function (req, res) {
   }
 })
 
+//GET quote from external API and set it as an attribute to the new employee 
+const setQuote = (newEmployee) => {
+  return request.get("https://ron-swanson-quotes.herokuapp.com/v2/quotes").then(
+    resp => {
+      newEmployee['quote'] = JSON.parse(resp)[0];
+      console.log(newEmployee)
+    }
+  )
+}
 
+//GET joke from external API and set it as an attribute to the new employee 
+const setJoke = (newEmployee) => {
+  return request.get({ url: "https://icanhazdadjoke.com", headers: { "Accept": "application/json" } }).then(resp => {
+    // let $ = cheerio.load(body)
+    // newEmployee['joke'] = $("p.subtitle").text();
+    newEmployee['joke'] = JSON.parse(resp)['joke'];
+    console.log(newEmployee)
+  })
+}
 
-/*POST new employee. Generate a unique id. If the role of the new employee is "CEO", reject it as the CEO already exists and there can be just one. The hire date of the employee has to be in the past, so has to be less than the date today. Add a joke and a quote to the employee's details*/
+//POST new employee. 
+//Generate a unique id.
+//If the role of the new employee is "CEO", reject it as the CEO already exists and there can be just one.
+//The hire date of the employee has to be in the past, so has to be less than the date today. 
+//Add a joke and a quote from the above functions to the employee's details
 router.post('', function (req, res) {
   maxId += 1;
   const newEmployee = req.body;
   if (newEmployee.role !== "CEO") {
     newEmployee.id = maxId;
     DATABASE["employee" + newEmployee.id] = newEmployee;
-
-    const url = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
-    request.get(url, (error, response, body) => {
-      newEmployee['quote'] = JSON.parse(body)[0];
-      const url1 = "https://icanhazdadjoke.com";
-      request.get(url1, (error, response, body) => {
-        let $ = cheerio.load(body)
-        newEmployee['joke'] = $("p.subtitle").text();
-        res.send(newEmployee)
-      })
+    Promise.all([setQuote(newEmployee), setJoke(newEmployee)]).then(resp => {
+      res.send(newEmployee)
     })
   }
   else { res.send("The role listed is wrong.") }
 }
 );
 
-/*UPDATE an employee's details. */
+//UPDATE an employee's details. 
 router.put('/:id', function (req, res) {
   const id = parseInt(req.params.id);
   const updatedEmployee = req.body;
